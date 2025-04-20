6f7c30371792ab9f75b23fe24105586b2571ca19
YazanAbj <axzs.yazan@gmail.com>
2025-04-20 11:13:48 +0300

    login and register with sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-//test
-//test2
-//
-//\//
-Route::get('/user', function (Request $request) {
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
